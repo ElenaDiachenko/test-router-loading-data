6e44ac77758fa90ac3bddf79b9b4474269b177da
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2023-03-09 00:46:14 +0200

    add base router

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import Home from 'pages/Home';
+import Error from 'pages/Error';
+import Pokemon from 'pages/Pokemon';
+import { Container } from '@mui/material';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Home />} path="/">
+      <Route element={<Pokemon />} path="pokemon/:name" />
+    </Route>,
+  ),
+);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Container maxWidth="md">
+      <RouterProvider router={router} />
+    </Container>
+  );
 }
 
-export default App
+export default App;
