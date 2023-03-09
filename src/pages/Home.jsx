2a2bbdae927be01a4fd3b9a5841582c5aeee96a3
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2023-03-09 04:36:58 +0200

    usage Await & defer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,43 +1,58 @@
-import { Link, useLoaderData } from 'react-router-dom';
+import { Suspense } from 'react';
+import { Link, useLoaderData, defer, Await } from 'react-router-dom';
 import { Grid, Typography, Card, CardContent } from '@mui/material';
 
-export const pokemonListLoader = async () => {
+const getPokemons = async () => {
   const results = await fetch('https://pokeapi.co/api/v2/pokemon');
 
   if (!results.ok) throw new Error('Something went wrong!');
 
-  const pokemons = await results.json();
-  return { pokemons };
+  const data = await results.json();
+  return data.results;
+};
+export const pokemonListLoader = async () => {
+  return defer({
+    pokemons: getPokemons(),
+  });
 };
 
 const Home = () => {
   const { pokemons } = useLoaderData();
+  console.log(pokemons);
   return (
     <Grid container spacing={3} py={2}>
-      {pokemons.results.map(({ name, url }) => (
-        <Grid item xs={12} sm={4} key={name}>
-          <Link to={`pokemon/${name}`}>
-            <Card
-              sx={{
-                color: 'inherit',
-                background: 'rgb(65 62 62 / 87%)',
-                display: 'flex',
-                justifyContent: 'center',
-                textAlign: 'center',
-                ':hover': {
-                  boxShadow: 10,
-                },
-              }}
-            >
-              <CardContent>
-                <Typography variant="h6" component="h3">
-                  {name}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Link>
-        </Grid>
-      ))}
+      <Suspense fallback={<h2>Loading ...</h2>}>
+        <Await resolve={pokemons}>
+          {(resolvedPokemons) => (
+            <>
+              {resolvedPokemons.map(({ name }) => (
+                <Grid item xs={12} sm={4} key={name}>
+                  <Link to={`pokemon/${name}`}>
+                    <Card
+                      sx={{
+                        color: 'inherit',
+                        background: 'rgb(65 62 62 / 87%)',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        textAlign: 'center',
+                        ':hover': {
+                          boxShadow: 10,
+                        },
+                      }}
+                    >
+                      <CardContent>
+                        <Typography variant="h6" component="h3">
+                          {name}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Link>
+                </Grid>
+              ))}
+            </>
+          )}
+        </Await>
+      </Suspense>
     </Grid>
   );
 };
